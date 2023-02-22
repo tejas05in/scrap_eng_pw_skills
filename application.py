928c858b8908e9d3b0ef59e97a28b8f9d4250bf1
tejas05in <tejas05in@yahoo.co.in>
2023-02-22 17:50:16 +0530

    first commit

--- application.py
+++ application.py
@@ -71,7 +71,8 @@ def index():
                 mydict = {"Product": searchString, "Name": name, "Rating": rating, "CommentHead": commentHead,
                           "Comment": custComment}
                 reviews.append(mydict)
-            client = pymongo.MongoClient("mongodb+srv://pwskills:pwskills@cluster0.ln0bt5m.mongodb.net/?retryWrites=true&w=majority")
+            
+            client = pymongo.MongoClient("mongodb+srv://tejas05in:sajetj@cluster0.cnbjrys.mongodb.net/?retryWrites=true&w=majority")
             db = client['review_scrap']
             review_col = db['review_scrap_data']
             review_col.insert_many(reviews)
